412650ca569b1d0dd783a626d4babdbe2db12aa0
jjh <jujihoon01@naver.com>
2020-03-01 15:48:57 +0900

    commit test

--- src/main/java/com/jjh/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/jjh/book/springboot/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.jjh.book.springboot.service.posts;
 
 import com.jjh.book.springboot.domain.posts.Posts;
 import com.jjh.book.springboot.domain.posts.PostsRepository;
+import com.jjh.book.springboot.web.dto.PostsListResponseDto;
 import com.jjh.book.springboot.web.dto.PostsResponseDto;
 import com.jjh.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jjh.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,6 +37,13 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(
                 () -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAll().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 
 }
