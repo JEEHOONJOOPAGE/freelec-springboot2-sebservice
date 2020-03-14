575ba2f32037fd7304c5bdba17d2e229460d18d5
jjh <jujihoon01@naver.com>
2020-03-14 23:57:46 +0900

    naver 로그인 개발완료 Application 에 application-oauth.properties 설정 어노테이션으로 직접 추가 test 다 삭제

--- src/main/java/com/jjh/book/springboot/Application.java
+++ src/main/java/com/jjh/book/springboot/Application.java
@@ -3,10 +3,12 @@ package com.jjh.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@PropertySource("application-oauth.properties")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/main/java/com/jjh/book/springboot/web/IndexController.java
+++ src/main/java/com/jjh/book/springboot/web/IndexController.java
@@ -6,30 +6,24 @@ import com.jjh.book.springboot.config.auth.dto.SessionUser;
 import com.jjh.book.springboot.service.posts.PostsService;
 import com.jjh.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
-import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(
-            Model model,
-            @LoginUser SessionUser user){
-
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts",postsService.findAllDesc());
-
-        if(user != null)
+        if(user != null){
             model.addAttribute("userName",user.getName());
-
+        }
         return "index";
     }
 
